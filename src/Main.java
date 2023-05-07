20f4f8c97d207b2c1275239a12d6663c76f05430
vadhub <gabderahmanov99@gmail.com>
2023-05-07 14:40:55 +0300

    data.txt

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         ArticleToXml articleToXml = new ArticleToXml.Builder()
+                //data.txt in scr
                 .setFilename("/home/vadim/math/data.txt")
-                .setFileOut("/home/vadim/math/data1.txt")
+                .setFileOut("/home/vadim/math/data.xml")
                 .build();
 
         articleToXml.convert();
